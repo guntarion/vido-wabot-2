04d48b9d02c50fc427746dcfd7d59cd31f38205e
Akhmad Guntar <guntar@me.com>
2024-04-27 21:54:57 +0700

    Versi modif yg working dari repo asli

--- vidobot.js
+++ vidobot.js
@@ -29,23 +29,62 @@ client.on('qr', (qr) => {
     console.log('QR RECEIVED', qr);
 });
 
-// client.on('authenticated', () => {
-//     console.log('AUTHENTICATED');
-// });
+client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
+});
 
-// client.on('auth_failure', (msg) => {
-//     // Fired if session restore was unsuccessful
-//     console.error('AUTHENTICATION FAILURE', msg);
-// });
+client.on('auth_failure', (msg) => {
+    // Fired if session restore was unsuccessful
+    console.error('AUTHENTICATION FAILURE', msg);
+});
 
 client.on('ready', () => {
     console.log('🧤 READY');
 });
 
+
+// SIMULATING TYPING AND DELAYED REPLIES
+
+async function replyWithDelay(chat, msg, replyText) {
+    // Simulate typing in the chat
+    chat.sendStateTyping();
+
+    // Wait for a random time between 2 and 4 seconds
+    const delay = Math.random() * 2000 + 2000;
+    setTimeout(() => {
+        msg.reply(replyText);
+    }, delay);
+}
+
+async function sendMessageWithDelay(chat, msg, messageText) {
+    // Simulate typing in the chat
+    chat.sendStateTyping();
+
+    // Wait for a random time between 2 and 4 seconds
+    const delay = Math.random() * 2000 + 2000;
+    setTimeout(() => {
+        client.sendMessage(msg.from, messageText);
+    }, delay);
+}
+
+
 client.on('message', async (msg) => {
     console.log('MESSAGE RECEIVED', msg);
+    const chat = await msg.getChat();
 
-    if (msg.body === '!ping reply') {
+    if (msg.body === '.status') {
+        const currentDate = new Date();
+        const masehiDateTime = currentDate.toLocaleString('en-US', {
+            timeZone: 'Asia/Jakarta',
+        });
+        const hijriDateTime = currentDate.toLocaleString('en-US', {
+            timeZone: 'Asia/Jakarta',
+            calendar: 'islamic-umalqura',
+        });
+
+        const replyMessage = `WA-Bot Vido is up and running 🚀\nMasehi: ${masehiDateTime}\nHijriah: ${hijriDateTime}`;
+        await replyWithDelay(chat, msg, replyMessage);
+    } else if (msg.body === '!ping reply') {
         // Send a new message as a reply to the current one
         msg.reply('pong');
     } else if (msg.body === '!ping') {
