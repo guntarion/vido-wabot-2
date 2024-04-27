5d4b43fe5ab16f6395e65f986fa24c03e865089f
Akhmad Guntar <guntar@me.com>
2024-04-27 22:17:51 +0700

    Add Express server and send message functionality via post request

--- vidobot.js
+++ vidobot.js
@@ -17,6 +17,17 @@ const client = new Client({
     },
 });
 
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const http = require('http');
+
+const app = express();
+app.use(bodyParser.json());
+const port = process.env.PORT || 3002;
+
+
+
 client.initialize();
 
 client.on('loading_screen', (percent, message) => {
@@ -40,6 +51,10 @@ client.on('auth_failure', (msg) => {
 
 client.on('ready', () => {
     console.log('🧤 READY');
+
+    /* Create HTTP server */
+    http.createServer(app).listen(port);
+    console.info('listening on port ' + port);
 });
 
 
@@ -109,3 +124,251 @@ client.on('message_create', async (msg) => {
         }
     }
 });
+
+
+
+
+/* Get endpoint to check current status  */
+app.get('/api/health', async (req, res) => {
+    res.json({
+        success: true,
+        message: 'Server is running healthy',
+    });
+});
+
+app.post('/send-message', (req, res) => {
+    const number = req.body.number;
+    const text = req.body.text;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendMessage(chatId, text);
+
+    res.send({ status: 'Message sent' });
+});
+
+app.post('/send-messages', (req, res) => {
+    const numbers = req.body.numbers; // Expect an array of numbers
+    const text = req.body.text;
+
+    numbers.forEach((number, index) => {
+        // Generate a random delay between 2 to 8 seconds
+        const delay = Math.floor(Math.random() * (8000 - 2000 + 1)) + 2000;
+
+        setTimeout(() => {
+            const chatId = number.substring(1) + '@c.us';
+            client.sendMessage(chatId, text);
+        }, index * delay);
+    });
+
+    res.send({ status: 'Messages sent' });
+});
+
+app.post('/send-unique-messages', (req, res) => {
+    const messages = req.body.messages; // Expect an array of objects { number: '123', text: 'Hello' }
+
+    messages.forEach((message, index) => {
+        // Generate a random delay between 2 to 8 seconds
+        const delay = Math.floor(Math.random() * (8000 - 2000 + 1)) + 2000;
+
+        setTimeout(() => {
+            const chatId = message.number.substring(1) + '@c.us';
+            client.sendMessage(chatId, message.text);
+        }, index * delay);
+    });
+
+    res.send({ status: 'Messages sent' });
+});
+
+app.post('/send-message-group', (req, res) => {
+    const number = req.body.number;
+    const text = req.body.text;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendMessage(chatId, text);
+
+    res.send({ status: 'Message sent' });
+});
+
+app.post('/send-media', (req, res) => {
+    const number = req.body.number;
+    const media = req.body.media;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendImage(chatId, media, 'media', 'Media caption');
+
+    res.send({ status: 'Media sent' });
+});
+
+app.post('/send-media-group', (req, res) => {
+    const number = req.body.number;
+    const media = req.body.media;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendImage(chatId, media, 'media', 'Media caption');
+
+    res.send({ status: 'Media sent' });
+});
+
+app.post('/send-location', (req, res) => {
+    const number = req.body.number;
+    const lat = req.body.lat;
+    const long = req.body.long;
+    const loc = req.body.loc;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendLocation(chatId, lat, long, loc);
+
+    res.send({ status: 'Location sent' });
+});
+
+app.post('/send-location-group', (req, res) => {
+    const number = req.body.number;
+    const lat = req.body.lat;
+    const long = req.body.long;
+    const loc = req.body.loc;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendLocation(chatId, lat, long, loc);
+
+    res.send({ status: 'Location sent' });
+});
+
+app.post('/send-contacts', (req, res) => {
+    const number = req.body.number;
+    const contact = req.body.contact;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendContact(chatId, contact);
+
+    res.send({ status: 'Contact sent' });
+});
+
+app.post('/send-contacts-group', (req, res) => {
+    const number = req.body.number;
+    const contact = req.body.contact;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendContact(chatId, contact);
+
+    res.send({ status: 'Contact sent' });
+});
+
+app.post('/send-link-preview', (req, res) => {
+    const number = req.body.number;
+    const link = req.body.link;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendMessage(chatId, link);
+
+    res.send({ status: 'Link sent' });
+});
+
+app.post('/send-link-preview-group', (req, res) => {
+    const number = req.body.number;
+    const link = req.body.link;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendMessage(chatId, link);
+
+    res.send({ status: 'Link sent' });
+});
+
+app.post('/send-voice', (req, res) => {
+    const number = req.body.number;
+    const voice = req.body.voice;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendVoice(chatId, voice);
+
+    res.send({ status: 'Voice sent' });
+});
+
+app.post('/send-voice-group', (req, res) => {
+    const number = req.body.number;
+    const voice = req.body.voice;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendVoice(chatId, voice);
+
+    res.send({ status: 'Voice sent' });
+});
+
+
+app.post('/send-video', (req, res) => {
+    const number = req.body.number;
+    const video = req.body.video;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendVideo(chatId, video, 'video', 'Video caption');
+
+    res.send({ status: 'Video sent' });
+});
+
+app.post('/send-video-group', (req, res) => {
+    const number = req.body.number;
+    const video = req.body.video;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendVideo(chatId, video, 'video', 'Video caption');
+
+    res.send({ status: 'Video sent' });
+});
+
+app.post('/send-document', (req, res) => {
+    const number = req.body.number;
+    const document = req.body.document;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendDocument(chatId, document, 'document', 'Document caption');
+
+    res.send({ status: 'Document sent' });
+});
+
+app.post('/send-document-group', (req, res) => {
+    const number = req.body.number;
+    const document = req.body.document;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendDocument(chatId, document, 'document', 'Document caption');
+
+    res.send({ status: 'Document sent' });
+});
+
+app.post('/send-sticker', (req, res) => {
+    const number = req.body.number;
+    const sticker = req.body.sticker;
+
+    const chatId = number.substring(1) + '@c.us';
+
+    client.sendImageAsSticker(chatId, sticker);
+
+    res.send({ status: 'Sticker sent' });
+});
+
+app.post('/send-sticker-group', (req, res) => {
+    const number = req.body.number;
+    const sticker = req.body.sticker;
+
+    const chatId = number.substring(1) + '@g.us';
+
+    client.sendImageAsSticker(chatId, sticker);
+
+    res.send({ status: 'Sticker sent' });
+});
+
